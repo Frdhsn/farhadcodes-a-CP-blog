38bfae4ddd696c60001f866989f0d31b027c8bc1
frdhsn <farhadiitju@gmail.com>
2022-06-12 03:09:02 +0600

    added global error handler. API bug exits

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,17 +1,16 @@
-const users = require("../controllers/userController.js");
-const express = require("express");
-var router = express.Router();
+const express = require('express');
+
+const users = require('../controllers/userController');
+
+const router = express.Router();
 
 // need to refactor
 
-exports.userrouter = () => {
-  router.
-    route("/")
-    .post(users.createUser)
-    .get(users.getAllUser);
-  router
-    .route("/:id")
+router.route('/').post(users.createUser).get(users.getAllUser);
+router
+    .route('/:id')
     .get(users.getUser)
     .put(users.updateUser)
     .delete(users.deleteUser);
-};
+
+module.exports = router;
--- services/userServices.js
+++ services/userServices.js
@@ -1,98 +1,63 @@
 class userServices {
-  constructor(table) {
-    this.user = table;
-  }
-  createUser = (req, res) => {
-    if (!req.body) {
-      res.status(400).send({
-        message: "Content can not be empty!",
-      });
-      return;
+    constructor(table) {
+        this.userTable = table;
     }
-
-    const newUser = {
-      name: req.body.name,
-      email: req.body.email,
-      password: req.body.password,
+    createUser = async (req, res) => {
+        const userBody = req.body;
+        const { name, email, password } = userBody;
+        const newUser = {
+            name,
+            email,
+            password
+        };
+        try {
+            const userData = await this.userTable.create(newUser);
+            return userData;
+        } catch (err) {
+            console.log(err);
+        }
     };
-    this.user
-      .create(newUser)
-      .then((data) => {
-        res.status(201).send(data);
-      })
-      .catch((err) => {
-        res.status(500).send({
-          message: err.message,
-        });
-      });
-  };
-  getUser = (req, res) => {
-    this.user
-      .findAll({ where: { id: req.params.id } })
-      .then((data) => {
-        res.status(200).send(data);
-      })
-      .catch((err) => {
-        res.status(500).send({
-          message: err.message,
-        });
-      });
-  };
-  getAllUser = (req, res) => {
-    this.user
-      .findAll()
-      .then((data) => {
-        res.status(200).send(data);
-      })
-      .catch((err) => {
-        res.status(500).send({
-          message: err.message,
-        });
-      });
-  };
-  updateUser = (req, res) => {
-    console.log(req.body);
-
-    const id = req.params.id;
-    this.user
-      .update(req.body, {
-        where: { id: id },
-      })
-      .then(() => {
-        res.status(200).send({
-          message: "Updated successfully!",
-        });
-      })
-      .catch((err) => {
-        res.status(500).send({
-          message: `Got an Error while updating story with id= ${id}`,
-        });
-      });
-  };
-  deleteUser = (req, res) => {
-    const id = req.params.id;
-    this.user
-      .destroy({
-        where: { id: id },
-      })
-      .then((num) => {
-        if(num){
-          res.status(200).send({
-            message: "User was deleted successfully!",
-          });
+    getUser = async (req, res) => {
+        const id = req.params.id;
+        try {
+            const userData = await this.userTable.findAll({
+                where: { id: id }
+            });
+            return userData;
+        } catch (err) {
+            console.log(err);
         }
-        else{
-          res.status(404).send({
-            message: "User doesn't exist!",
-          });
+    };
+    getAllUser = async (req, res) => {
+        try {
+            const userData = await this.userTable.findAll();
+            return userData;
+        } catch (err) {
+            console.log(err);
         }
-      })
-      .catch((err) => {
-        res.status(500).send({
-          message: `Could not delete user with id= ${id}`,
-        });
-      });
-  };
+    };
+    updateUser = async (req, res) => {
+        const id = req.params.id;
+        try {
+            const userData = await this.userTable.update(req.body, {
+                where: { id: id }
+            });
+            return userData;
+        } catch (err) {
+            console.log(err);
+        }
+    };
+    deleteUser = async (req, res) => {
+        const id = req.params.id;
+        try {
+            const userData = await this.userTable.destroy({
+                where: { id: id }
+            });
+            return userData;
+        } catch (err) {
+            console.log(err);
+        }
+    };
 }
 
 module.exports = userServices;
