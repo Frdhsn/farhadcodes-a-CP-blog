216cea985a992dd8a69b2647c312f3e9a08a3614
frdhsn <farhadiitju@gmail.com>
2022-06-17 18:57:03 +0600

    user route protection improved based on #4

--- utils/generateToken.js
+++ utils/generateToken.js
@@ -1,10 +1,5 @@
 const jwt = require('jsonwebtoken');
 
-// exports.signToken = (email) => {
-//   return jwt.sign({ email }, process.env.JWT_SECRET, {
-//     expiresIn: process.env.JWT_EXPIRES_IN,
-//   });
-// };
 exports.signToken = (id) => {
   return jwt.sign({ id }, process.env.JWT_SECRET, {
     expiresIn: process.env.JWT_EXPIRES_IN,
